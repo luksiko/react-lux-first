b472712fc9ed6208e319bdbad461d818ff15f5b7
Aleksey <luksiko90@gmail.com>
2020-08-15 12:35:06 +0500

    Add files via upload

---
+++ build/precache-manifest.9fb2199329225306a00be45125dbf056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c78ca8e7d508d100067fdef36935975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d54ad3a4d83bd4a1c56",
+    "url": "/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "27329ebeb7bbd21cd7de",
+    "url": "/static/js/2.35bd22d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35bd22d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d54ad3a4d83bd4a1c56",
+    "url": "/static/js/main.497d9541.chunk.js"
+  },
+  {
+    "revision": "2876a4acc5930c7aec89",
+    "url": "/static/js/runtime-main.55fd8d41.js"
+  }
+]);

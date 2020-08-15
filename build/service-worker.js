6e956c64bb040464aa659c6f2d179cb9bd6403f4
Aleksey <luksiko90@gmail.com>
2020-08-15 13:10:57 +0500

    deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9fb2199329225306a00be45125dbf056.js"
+  "/precache-manifest.480ed088c7050e682504060d72b91e49.js"
 );
 
 self.addEventListener('message', (event) => {
